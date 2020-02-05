c0c93b526a7e7abfb35f2433de32c22a23a05448
SqpuHkS <kovaleksey01@gmail.com>
2020-02-05 23:41:31 +0200

    added functionality to delete posts and tags

--- site_blog/utils.py
+++ site_blog/utils.py
@@ -47,4 +47,21 @@ class ObjectUpdateMixin:
         if bound_form.is_valid():
             new_obj = bound_form.save()
             return redirect(new_obj)
-        return render(request, self.template, context={self.model.__name__.lower: obj, 'form': bound_form})
+        return render(request, self.template, context={self.model.__name__.lower(): obj, 'form': bound_form})
+
+
+class ObjectDeleteMixin:
+    model = None
+    template = None
+    redirect_url = None
+
+    def get(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        return render(request, self.template, context={self.model.__name__.lower(): obj})
+
+    def post(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        obj.delete()
+        return redirect(reverse(self.redirect_url))
+
+
--- site_blog/views.py
+++ site_blog/views.py
@@ -42,3 +42,13 @@ class TagCreate(ObjectCreateMixin, View):
 class PostCreate(ObjectCreateMixin, View):
     model_form = PostForm
     template = 'blog/post_create_form.html'
+
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete.html'
+    redirect_url = 'posts_list_url'
+
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete.html'
+    redirect_url = 'tags_list_url'
